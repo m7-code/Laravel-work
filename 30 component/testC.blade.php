be027abec1cfeb60ca91b3d1d77846e04d143ee8
m7-code <mughiraasad6@gmail.com>
2026-02-03 07:30:34 -0800

    adding laravel work

---
+++ 30 component/testC.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>component</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+    
+    <x-alert type="success" message="Your operation was successful!" />
+    <x-alert type="danger" message="There was an error processing your request." />
+    <x-alert type="warning" message="Please be cautious while proceeding." />
+    <x-alert type="info" message="This is some important information for you." />
+
+
+</body>
+</html>
